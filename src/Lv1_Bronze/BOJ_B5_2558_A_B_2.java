2f4b61e8a0b622b23be999c0fa516a919f643e83
HoonSikE <hoongrammer@gmail.com>
2022-04-09 13:36:03 +0900

    Create BOJ_B5_2558_A_B_2.java

---
+++ src/Lv1_Bronze/BOJ_B5_2558_A_B_2.java
@@ -0,0 +1,16 @@
+package Lv1_Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * @author HanHoon
+ * @category
+ * https://www.acmicpc.net/problem/2558
+ */
+public class BOJ_B5_2558_A_B_2 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+	}
+}
