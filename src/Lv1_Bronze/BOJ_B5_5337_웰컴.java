cea86f00150c8b11bd99686c9423ae02ae17631e
HoonSikE <hoongrammer@gmail.com>
2022-04-10 23:41:28 +0900

    Create BOJ_B5_5337_웰컴.java

---
+++ src/Lv1_Bronze/BOJ_B5_5337_웰컴.java
@@ -0,0 +1,13 @@
+package Lv1_Bronze;
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/5337
+ */
+public class BOJ_B5_5337_웰컴 {
+	public static void main(String[] args) {
+		System.out.println(".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
