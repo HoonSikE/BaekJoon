a3d5b7959db70428b026fa3b6098b87507ecd351
HoonSikE <hoongrammer@gmail.com>
2022-04-10 23:41:30 +0900

    Create BOJ_B5_10172_개.java

---
+++ src/Lv1_Bronze/BOJ_B5_10172_개.java
@@ -0,0 +1,15 @@
+package Lv1_Bronze;
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/10172
+ */
+public class BOJ_B5_10172_개 {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+	}
+}
