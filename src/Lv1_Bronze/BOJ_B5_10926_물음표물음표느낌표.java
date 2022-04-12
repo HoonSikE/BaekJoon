39c1ee9599b0ed6c769e3bc816d673b9240281ad
HoonSikE <hoongrammer@gmail.com>
2022-04-12 21:28:26 +0900

    Create BOJ_B5_10926_물음표물음표느낌표.java

---
+++ src/Lv1_Bronze/BOJ_B5_10926_물음표물음표느낌표.java
@@ -0,0 +1,16 @@
+package Lv1_Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/10926
+ */
+public class BOJ_B5_10926_물음표물음표느낌표 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine() + "??!");
+	}
+}
