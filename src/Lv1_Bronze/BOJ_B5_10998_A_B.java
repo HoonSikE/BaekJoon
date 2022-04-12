67ee96dd26d176056ab32b8bea03972cce52c8c7
HoonSikE <hoongrammer@gmail.com>
2022-04-12 21:28:24 +0900

    Create BOJ_B5_10998_A_B.java

---
+++ src/Lv1_Bronze/BOJ_B5_10998_A_B.java
@@ -0,0 +1,19 @@
+package Lv1_Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+/**
+ * @author HanHoon
+ * @category 수학, 구현, 사칙연산
+ * https://www.acmicpc.net/problem/10998
+ */
+public class BOJ_B5_10998_A_B {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		System.out.println(Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+		br.close();
+	}
+}
