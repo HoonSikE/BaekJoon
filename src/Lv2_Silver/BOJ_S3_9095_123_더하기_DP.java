c01b07813b3d55897a478e231f873de7a2cf04e2
HoonSikE <hoongrammer@gmail.com>
2022-04-14 10:51:12 +0900

    Update BOJ_S3_9095_123_더하기_DP.java

--- src/Lv2_Silver/BOJ_S3_9095_123_더하기_DP.java
+++ src/Lv2_Silver/BOJ_S3_9095_123_더하기_DP.java
@@ -9,21 +9,21 @@ public class BOJ_S3_9095_123_더하기_DP {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder str = new StringBuilder();
 		// 테스트 케이스 개수 T
-//		int T = Integer.parseInt(br.readLine());
-//		int[] dp = new int[10];
-//		dp[0] = 1;
-//		
-//		for (int i = 1; i < 10; i++) {
-//			for (int j = 1; j < 4; j++) {
-//				if(i-j < 0)
-//					break;
-//				dp[i] += dp[i-j];
-//			}
-//		}
-//		while(T-->0) {
-//			str.append(dp[Integer.parseInt(br.readLine())-1]).append("\n");
-//		}
-//		System.out.println(str.toString());
-//		br.close();
+		int T = Integer.parseInt(br.readLine());
+		int[] dp = new int[10];
+		dp[0] = 1;
+		
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 4; j++) {
+				if(i-j < 0)
+					break;
+				dp[i] += dp[i-j];
+			}
+		}
+		while(T-->0) {
+			str.append(dp[Integer.parseInt(br.readLine())-1]).append("\n");
+		}
+		System.out.println(str.toString());
+		br.close();
 	}
 }
