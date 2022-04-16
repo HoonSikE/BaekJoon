85687910080dc0de016beef67d47f6b634a21990
HoonSikE <hoongrammer@gmail.com>
2022-04-16 14:50:55 +0900

    Create BOJ_B5_18108_1998년생인_내가_태국에서는_2541년생.java

---
+++ src/Lv1_Bronze/BOJ_B5_18108_1998년생인_내가_태국에서는_2541년생.java
@@ -0,0 +1,14 @@
+package Lv1_Bronze;
+
+import java.util.Scanner;
+/**
+ * @author HanHoon
+ * @category 수학, 사칙연산
+ * https://www.acmicpc.net/problem/18108
+ */
+public class BOJ_B5_18108_1998년생인_내가_태국에서는_2541년생 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()-543);
+	}
+}
