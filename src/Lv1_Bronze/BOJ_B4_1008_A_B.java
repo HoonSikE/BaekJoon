874760be84d9de3d2780ba7cbd0c82810ef2127c
HoonSikE <hoongrammer@gmail.com>
2022-04-16 14:50:48 +0900

    Create BOJ_B4_1008_A_B.java

---
+++ src/Lv1_Bronze/BOJ_B4_1008_A_B.java
@@ -0,0 +1,14 @@
+package Lv1_Bronze;
+
+import java.util.Scanner;
+/**
+ * @author HanHoon
+ * @category 수학, 구현, 사칙연산
+ * https://www.acmicpc.net/problem/1008
+ */
+public class BOJ_B4_1008_A_B {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextDouble() / sc.nextDouble());
+	}
+}
