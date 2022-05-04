eb20ed41542ce6633af4d8ef7aca14b1d7cfd976
HoonSikE <hoongrammer@gmail.com>
2022-05-04 15:22:01 +0900

    Create BOJ_B5_25083_새싹.java

---
+++ src/Lv1_Bronze/BOJ_B5_25083_새싹.java
@@ -0,0 +1,16 @@
+package Lv1_Bronze;
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/25083
+ */
+public class BOJ_B5_25083_새싹 {
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7\n" + 
+							"r`-_   ,'  ,/\n" + 
+							" \\. \". L_r'\n" + 
+							"   `~\\/\n" + 
+							"      |\n" + 
+							"      |");
+	}
+}
