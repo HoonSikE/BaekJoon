d74a6334af8a968fa16f22c3df4487e28d1ec9b0
HoonSikE <hoongrammer@gmail.com>
2022-05-17 22:25:02 +0900

    Create BOJ_B3_8393_합.java

---
+++ src/Lv1_Bronze/BOJ_B3_8393_합.java
@@ -0,0 +1,18 @@
+package Lv1_Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/**
+ * @author HanHoon
+ * @category 수학, 구현
+ * https://www.acmicpc.net/problem/8393
+ */
+public class BOJ_B3_8393_합 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n*(n+1)/2);
+		br.close();
+	}
+}
