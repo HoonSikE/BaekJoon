786d8dd066b5557f5c3ac70118a3f0112a18b714
HanHoon <hoongrammer@gmail.com>
2023-01-15 18:44:08 +0900

    Update BOJ 2023-01-15

---
+++ src/boj5597.java
@@ -0,0 +1,17 @@
+import java.util.*;
+https://www.google.com/search?q=%EB%B0%B1%EC%A4%80+%EC%9E%90%EB%B0%94+5597&sxsrf=AJOqlzVstTJxpb2_fkgWmheMQDg28Wubqw%3A1673662800424&ei=UBHCY6DIGbLDpgfdlLmYAg&ved=0ahUKEwigsMXl_8X8AhWyoekKHV1KDiMQ4dUDCA8&uact=5&oq=%EB%B0%B1%EC%A4%80+%EC%9E%90%EB%B0%94+5597&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQgAQ6CggAEEcQ1gQQsAM6BAgAEB46BggAEAgQHkoECEEYAEoECEYYAFDsDViRFGCpFWgDcAF4AIABqgGIAd8DkgEDMC4zmAEAoAEByAEKwAEB&sclient=gws-wiz-serp
+public class Baekjoon5597 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] student = new int[31];
+		for(int i=1; i<29; i++) {
+			int success = sc.nextInt();
+			student[success] = 1;
+		}
+		for(int i=1; i<student.length; i++) {
+			if(student[i]!=1)
+				System.out.println(i);
+		}
+		sc.close();
+	}
+}
