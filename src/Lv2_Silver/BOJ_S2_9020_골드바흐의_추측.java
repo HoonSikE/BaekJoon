83eab4c0c71314c55a9cc8df34ac130ba3046db4
HanHoon <hoongrammer@gmail.com>
2023-02-12 16:18:52 +0900

    Update BOJ 2023-02-12

---
+++ src/Lv2_Silver/BOJ_S2_9020_골드바흐의_추측.java
@@ -0,0 +1,29 @@
+package Lv2_Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author HanHoon
+ * @category 수학
+ * https://www.acmicpc.net/problem/3003
+ */
+public class BOJ_S2_9020_골드바흐의_추측 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder str = new StringBuilder();
+
+        // 테스트 케이스 개수 T
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++){
+            st = new StringTokenizer(br.readLine());
+            str.append("Case #").append(i).append(": ").append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+        }
+        System.out.print(str);
+        br.close();
+    }
+}
