dbb3d548dbdfebe97f32cec38294427ef4f907a7
HanHoon <hoongrammer@gmail.com>
2023-02-18 18:09:15 +0900

    Update BOJ 2023-02-18

--- src/Lv3_Gold/BOJ_G5_2251_물통.java
+++ src/Lv3_Gold/BOJ_G5_2251_물통.java
@@ -8,30 +8,84 @@ import java.util.*;
  * https://www.acmicpc.net/problem/2251
  */
 public class BOJ_G5_2251_물통 {
+    static int A, B, C;
+    static ArrayList<Integer> result = new ArrayList<>();
+    static class Bottle{
+        int a;
+        int b;
+        int c;
+
+        public Bottle(int a, int b, int c){
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+    public static void bfs(){
+        Queue<Bottle> queue = new LinkedList<>();
+        boolean[][][] isVisited = new boolean[A+1][B+1][C+1];
+
+        // 초기 상태
+        queue.add(new Bottle(0, 0, C));
+
+        while(!queue.isEmpty()){
+            Bottle current = queue.poll();
+            // 현재 상태에서 체크한 적이 있다면 continue;
+            if(isVisited[current.a][current.b][current.c]) continue;
+
+            // 첫 번째 물통이 비어 있을 때, 세 번째 물통에 담겨있을 수 있는 물의 양 추가
+            if(current.a == 0)
+                result.add(current.c);
+
+            isVisited[current.a][current.b][current.c] = true;
+
+            // A->B
+            if (current.a + current.b <= B)
+                queue.add(new Bottle(0, current.a + current.b, current.c));
+            else
+                queue.add(new Bottle(current.a + current.b - B, B, current.c));
+            // A->C
+            if (current.a + current.c <= C)
+                queue.add(new Bottle(0, current.b, current.a + current.c));
+            else
+                queue.add(new Bottle(current.a + current.c - C, current.b, C));
+            // B->A
+            if (current.a + current.b <= A)
+                queue.add(new Bottle(current.a + current.b, 0, current.c));
+            else
+                queue.add(new Bottle(A, current.a + current.b - A, current.c));
+            // B->C
+            if (current.b + current.c <= C)
+                queue.add(new Bottle(current.a, 0, current.b + current.c));
+            else
+                queue.add(new Bottle(current.a, current.b + current.c - C, C));
+            // C->A
+            if (current.a + current.c <= A)
+                queue.add(new Bottle(current.a + current.c, current.b, 0));
+            else
+                queue.add(new Bottle(A, current.b, current.a + current.c - A));
+            // C->B
+            if (current.b + current.c <= B)
+                queue.add(new Bottle(current.a, current.b + current.c, 0));
+            else
+                queue.add(new Bottle(current.a, B, current.b + current.c - B));
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
-        int N = 3;
-        int[] num = new int[N];
-        for (int i = 0; i < N; i++)
-            num[i] = Integer.parseInt(st.nextToken());
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++)
-                set.add(num[j] - num[i]);
-            set.add(num[i]);
-        }
-        for (int i = 1; i <= num[2]; i++)
-            if(set.contains(i))
-                if((num[2] - i) <= num[1])
-                    str.append(i).append(" ");
+        // 세 정수 A, B, C (물병의 최대 용량)
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-/*        // 세 정수 A, B, C
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());*/
+        bfs();
+
+        Collections.sort(result);
+        for(int n : result)
+            str.append(n + " ");
 
         System.out.print(str);
         br.close();
