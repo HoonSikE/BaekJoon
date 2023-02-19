4a4cd95908006c9ff628a91c16524ac21e40607c
HanHoon <hoongrammer@gmail.com>
2023-02-19 21:33:28 +0900

    Update BOJ 2023-02-19

--- src/Lv3_Gold/BOJ_G5_2015_수들의_합_4.java
+++ src/Lv3_Gold/BOJ_G5_2015_수들의_합_4.java
@@ -1,49 +1,42 @@
 package Lv3_Gold;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 /**
  * @author HanHoon
  * @category 자료 구조, 누적 합, 트리를 사용한 집합과 맵
  * https://www.acmicpc.net/problem/2015
  */
 public class BOJ_G5_2015_수들의_합_4 {
-	static int K, result;
-	public static void combination(int start, int count, int sum, ArrayList<Integer> list) {
-		if(count == 2) {
-			if(sum == K) result++;
-			return;
-		}
-		for (int i = start; i < list.size(); i++) {
-			// sort를 했기때문에 어차피 값보다 크다면 break;
-			sum += list.get(i);
-			if(sum > K) break;
-			combination(i+1, count+1, sum, list);
-			sum -= list.get(i);
-		}
-	}
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		// N개의 수
 		int N = Integer.parseInt(st.nextToken());
 		// 합이 K인 숫자
-		K = Integer.parseInt(st.nextToken());
-		
-		ArrayList<Integer> list = new ArrayList<>();
-		result = 0;
+		int K = Integer.parseInt(st.nextToken());
+		long result = 0;
+
+		Map<Integer, Long> map = new HashMap<>();
+		int[] arr = new int[N+1];
+
 		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(st.nextToken()));
+		for (int i = 1; i <= N; i++) {
+			// 부분합 구하기
+			arr[i] = Integer.parseInt(st.nextToken()) + arr[i-1];
+
+			if(arr[i] == K)
+				result++;
+
+			if(map.containsKey(arr[i] - K))
+				result += map.get(arr[i] -K);
+
+			if(!map.containsKey(arr[i]))
+				map.put(arr[i], 1l);
+			else
+				map.put(arr[i], map.get(arr[i]) + 1);
 		}
-		Collections.sort(list);
-		
-		combination(0, 0, 0, list);
-		
+
 		System.out.println(result);
 		br.close();
 	}
