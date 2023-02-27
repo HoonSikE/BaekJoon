180db0673a4597574de52317e94d35490416aa32
HanHoon <hoongrammer@gmail.com>
2023-02-27 23:03:23 +0900

    Update BOJ 2023-02-27

---
+++ src/Lv1_Bronze/BOJ_B5_2743_단어_길이_재기.java
@@ -0,0 +1,30 @@
+package Lv1_Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author HanHoon
+ * @category .
+ * https://www.acmicpc.net/problem/.
+ */
+public class BOJ_B5_2743_단어_길이_재기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder str = new StringBuilder();
+
+        // N
+        int N = Integer.parseInt(br.readLine());
+
+        for (int n = 0; n < N; n++){
+            st = new StringTokenizer(br.readLine());
+            Integer.parseInt(st.nextToken());
+            Integer.parseInt(br.readLine());
+        }
+        System.out.print(str);
+        br.close();
+    }
+}
