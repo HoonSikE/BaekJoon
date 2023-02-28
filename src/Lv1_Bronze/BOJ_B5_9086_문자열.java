29f75b7d03e23919911a8b32685a0c28963128f6
HanHoon <hoongrammer@gmail.com>
2023-02-28 22:22:06 +0900

    Update BOJ 2023-02-28

--- src/Lv1_Bronze/BOJ_B5_9086_문자열.java
+++ src/Lv1_Bronze/BOJ_B5_9086_문자열.java
@@ -4,15 +4,20 @@ import java.io.*;
 /**
  * @author HanHoon
  * @category 구현, 문자열
- * https://www.acmicpc.net/problem/2743
+ * https://www.acmicpc.net/problem/9086
  */
-public class BOJ_B5_2743_단어_길이_재기 {
+public class BOJ_B5_9086_문자열 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder str = new StringBuilder();
 
-        str.append(br.readLine().length());
+        // 테스트 케이스 개수 T
+        int T = Integer.parseInt(br.readLine());
 
+        for (int t = 0; t < T; t++){
+            String tmp = br.readLine();
+            str.append(tmp.charAt(0)).append(tmp.charAt(tmp.length()-1)).append("\n");
+        }
         System.out.print(str);
         br.close();
     }
