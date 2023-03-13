a390342877cce3678c8e23a51263a1715534ebfc
HanHoon <hoongrammer@gmail.com>
2023-03-13 11:41:11 +0900

    Update BOJ 2023-03-13

--- src/Lv2_Silver/BOJ_S3_11478_서로_다른_문자열의_개수.java
+++ src/Lv2_Silver/BOJ_S3_11478_서로_다른_문자열의_개수.java
@@ -1,13 +1,16 @@
 package Lv2_Silver;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /**
  * @author HanHoon
  * @category .
  * https://www.acmicpc.net/problem/.
  */
-public class BOJ_S4_1269_대칭_차집합 {
+public class BOJ_S3_11478_서로_다른_문자열의_개수 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
