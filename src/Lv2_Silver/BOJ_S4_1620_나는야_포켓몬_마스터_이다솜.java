ecc62a1843d90e192289cff152564dde9fbb488b
HanHoon <hoongrammer@gmail.com>
2023-03-14 22:18:12 +0900

    Update BOJ 2023-03-14

---
+++ src/Lv2_Silver/BOJ_S4_1620_나는야_포켓몬_마스터_이다솜.java
@@ -0,0 +1,30 @@
+package Lv2_Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author HanHoon
+ * @category .
+ * https://www.acmicpc.net/problem/.
+ */
+public class BOJ_S4_1620_나는야_포켓몬_마스터_이다솜 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder str = new StringBuilder();
+
+        // N
+        int N = Integer.parseInt(br.readLine());
+
+        for (int n = 0; n < N; n++){
+            st = new StringTokenizer(br.readLine());
+            Integer.parseInt(st.nextToken());
+            Integer.parseInt(br.readLine());
+        }
+        System.out.print(str);
+        br.close();
+    }
+}
