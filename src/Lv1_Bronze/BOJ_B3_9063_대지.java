bdded08a89538f07fcb8f7ca859c5f7eaaa09324
HanHoon <hoongrammer@gmail.com>
2023-03-27 23:42:39 +0900

    Update BOJ 2023-03-27

--- src/Lv1_Bronze/BOJ_B3_9063_대지.java
+++ src/Lv1_Bronze/BOJ_B3_9063_대지.java
@@ -1,13 +1,10 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 /**
  * @author HanHoon
- * @category .
+ * @category 수학, 구현, 기하학
  * https://www.acmicpc.net/problem/.
  */
 public class BOJ_B3_9063_대지 {
@@ -16,14 +13,25 @@ public class BOJ_B3_9063_대지 {
         StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
-        // N
+        // N: 점의 개수
         int N = Integer.parseInt(br.readLine());
+
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
 
         for (int n = 0; n < N; n++){
             st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            minX = minX > x ? x : minX;
+            maxX = maxX < x ? x : maxX;
+            int y = Integer.parseInt(st.nextToken());
+            minY = minY > y ? y : minY;
+            maxY = maxY < y ? y : maxY;
         }
+        str.append((maxY-minY)*(maxX-minX));
+
         System.out.print(str);
         br.close();
     }
