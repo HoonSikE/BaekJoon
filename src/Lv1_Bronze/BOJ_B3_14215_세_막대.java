9b4e489a32a2bee1cec75590590f0262b0f5db58
HanHoon <hoongrammer@gmail.com>
2023-04-18 15:32:24 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B3_14215_세_막대.java
+++ src/Lv1_Bronze/BOJ_B3_14215_세_막대.java
@@ -1,27 +1,27 @@
 package Lv1_Bronze;
 
 import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category 수학
- * https://www.acmicpc.net/problem/2903
+ * @category 수학, 구현, 기하학
+ * https://www.acmicpc.net/problem/14215
  */
-public class BOJ_B3_2903_중앙_이동_알고리즘 {
+public class BOJ_B3_14215_세_막대 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
 
-        int width = 3;
-
-        for(int n = 1; n < N; n++)
-            width = width*2 - 1;
-
-        str.append(width*width);
-
+        for (int n = 0; n < N; n++){
+            st = new StringTokenizer(br.readLine());
+            Integer.parseInt(st.nextToken());
+            Integer.parseInt(br.readLine());
+        }
         System.out.print(str);
         br.close();
     }
--- src/Lv2_Silver/BOJ_S5_25206_너의_평점은.java
+++ src/Lv2_Silver/BOJ_S5_25206_너의_평점은.java
@@ -5,24 +5,65 @@ import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 수학, 구현, 문자열
+ * https://www.acmicpc.net/problem/25206
  */
 public class BOJ_S5_25206_너의_평점은 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        // 학점 총합
+        double sum = 0;
+        // 과목 수
+        double cnt = 0;
 
-        for (int n = 0; n < N; n++){
+        for (int n = 0; n < 20; n++){
             st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            st.nextToken();
+            // 학점
+            double credit = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
+
+            switch(grade.charAt(0)){
+                case 'P':
+                    break;
+                case 'F':
+                    cnt++;
+                    break;
+                case 'A':
+                    if(grade.charAt(1) == '+')
+                        sum += 4.5 * credit;
+                    else
+                        sum += 4.0 * credit;
+                    cnt += credit;
+                    break;
+                case 'B':
+                    if(grade.charAt(1) == '+')
+                        sum += 3.5 * credit;
+                    else
+                        sum += 3.0 * credit;
+                    cnt += credit;
+                    break;
+                case 'C':
+                    if(grade.charAt(1) == '+')
+                        sum += 2.5 * credit;
+                    else
+                        sum += 2.0 * credit;
+                    cnt += credit;
+                    break;
+                case 'D':
+                    if(grade.charAt(1) == '+')
+                        sum += 1.5 * credit;
+                    else
+                        sum += 1.0 * credit;
+                    cnt += credit;
+                    break;
+            }
         }
-        System.out.print(str);
+
+        System.out.printf("%.6f\n", sum/cnt);
+
         br.close();
     }
 }
