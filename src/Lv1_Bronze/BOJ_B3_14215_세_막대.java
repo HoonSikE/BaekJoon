eeed058d7349069bf9515bd8ae71f383f80ec04c
HanHoon <hoongrammer@gmail.com>
2023-04-19 14:30:47 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B3_14215_세_막대.java
+++ src/Lv1_Bronze/BOJ_B3_14215_세_막대.java
@@ -11,17 +11,23 @@ import java.util.*;
 public class BOJ_B3_14215_세_막대 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
-        }
+        int N = 3;
+
+        while(N --> 0)
+            list.add(Integer.parseInt(st.nextToken()));
+
+        Collections.sort(list);
+
+        if(list.get(0) + list.get(1) > list.get(2))
+            str.append(list.get(0) + list.get(1) + list.get(2));
+        else
+            str.append((list.get(0) + list.get(1)) * 2 - 1);
+
         System.out.print(str);
         br.close();
     }
