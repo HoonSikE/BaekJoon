60ca8e3bbf1102d5f993755c846fbbf5b27499d6
HanHoon <hoongrammer@gmail.com>
2023-05-01 23:34:44 +0900

    Update BOJ 2023-05-01

--- src/Lv2_Silver/BOJ_S4_2164_카드2.java
+++ src/Lv2_Silver/BOJ_S4_2164_카드2.java
@@ -17,11 +17,20 @@ public class BOJ_S4_2164_카드2 {
         // N
         int N = Integer.parseInt(br.readLine());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int n = 1; n <= N; n++)
+            queue.offer(n);
+
+        while(queue.size() > 1){
+            // 우선, 제일 위에 있는 카드를 바닥에 버린다.
+            queue.poll();
+            // 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옯긴다.
+            queue.offer(queue.poll());
         }
+
+        str.append(queue.poll());
+
         System.out.print(str);
         br.close();
     }
