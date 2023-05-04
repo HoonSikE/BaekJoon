df82558de0f1ee12ee690d4c4ea853e152eb3f16
HanHoon <hoongrammer@gmail.com>
2023-05-04 15:51:10 +0900

    Update BOJ 2023-05-04

--- src/Lv2_Silver/BOJ_S5_7785_회사에_있는_사람.java
+++ src/Lv2_Silver/BOJ_S5_7785_회사에_있는_사람.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * @author HanHoon
  * @category 자료 구조, 해시를 사용한 집합과 맵
- * https://www.acmicpc.net/problem/.
+ * https://www.acmicpc.net/problem/7785
  */
 public class BOJ_S5_7785_회사에_있는_사람 {
     public static void main(String[] args) throws IOException {
@@ -15,12 +15,32 @@ public class BOJ_S5_7785_회사에_있는_사람 {
 
         // N
         int N = Integer.parseInt(br.readLine());
+        HashMap<String, String> map = new HashMap<>();
 
         for (int n = 0; n < N; n++){
             st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            String key = st.nextToken();
+            String value = st.nextToken();
+
+            if(value.equals("enter"))
+                map.put(key, value);
+            else if(value.equals("leave"))
+                if(map.containsKey(key))
+                    map.remove(key);
         }
+
+        ArrayList<String> list = new ArrayList<String>(map.keySet());
+
+        /**
+         * reverse
+         * reverseOrder
+         */
+//        Collections.reverse(list);
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(String s : list)
+            str.append(s).append("\n");
+
         System.out.print(str);
         br.close();
     }
