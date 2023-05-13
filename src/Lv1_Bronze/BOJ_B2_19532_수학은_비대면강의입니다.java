be819cc5ef59b7bc19dae6ed2193921587010347
HanHoon <hoongrammer@gmail.com>
2023-05-13 16:09:03 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B2_19532_수학은_비대면강의입니다.java
+++ src/Lv1_Bronze/BOJ_B2_19532_수학은_비대면강의입니다.java
@@ -1,29 +1,27 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
+ * @category 수학, 브루트포스 알고리즘
  * https://www.acmicpc.net/problem/19532
  */
 public class BOJ_B2_19532_수학은_비대면강의입니다 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
         // N
-        int N = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
-        }
         System.out.print(str);
         br.close();
     }
