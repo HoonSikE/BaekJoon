f536e1941cc794e25b55d7c92490ca7b894ea505
HanHoon <hoongrammer@gmail.com>
2023-05-16 21:07:26 +0900

    Update BOJ 10816

--- src/Lv2_Silver/BOJ_S4_10816_숫자_카드_2.java
+++ src/Lv2_Silver/BOJ_S4_10816_숫자_카드_2.java
@@ -1,14 +1,12 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 자료 구조, 정렬, 이분 탐색, 해시를 사용한 집합과 맵
+ * https://www.acmicpc.net/problem/10816
  */
 public class BOJ_S4_10816_숫자_카드_2 {
     public static void main(String[] args) throws IOException {
@@ -16,14 +14,30 @@ public class BOJ_S4_10816_숫자_카드_2 {
         StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
-        // N
+        // N: 상근이가 가지고 있는 숫자 카드의 개수
         int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        st = new StringTokenizer(br.readLine());
         for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            if(map.containsKey(num))
+                map.put(num, map.get(num)+1);
+            else
+                map.put(num, 1);
         }
+
+        int M = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for(int m = 0; m < M; m++){
+            int num = Integer.parseInt(st.nextToken());
+            if(map.containsKey(num))
+                str.append(map.get(num) + " ");
+            else
+                str.append(0 + " ");
+        }
+
         System.out.print(str);
         br.close();
     }
