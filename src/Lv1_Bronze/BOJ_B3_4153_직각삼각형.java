c147212e87e3fb36b7ddafe3d21bc85335a9e21e
HanHoon <hoongrammer@gmail.com>
2023-05-16 21:25:12 +0900

    Update BOJ 4153

--- src/Lv1_Bronze/BOJ_B3_4153_직각삼각형.java
+++ src/Lv1_Bronze/BOJ_B3_4153_직각삼각형.java
@@ -1,14 +1,11 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 수학, 기하학, 피타고라스 정리
+ * https://www.acmicpc.net/problem/4153
  */
 public class BOJ_B3_4153_직각삼각형 {
     public static void main(String[] args) throws IOException {
@@ -16,14 +13,23 @@ public class BOJ_B3_4153_직각삼각형 {
         StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
-
-        for (int n = 0; n < N; n++){
+        while(true){
             st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            int[] arr = new int[3];
+            for(int i = 0; i < arr.length; i++)
+                arr[i] = Integer.parseInt(st.nextToken());
+
+            if(arr[0] == 0 && arr[1] == 0 && arr[2] == 0)
+                break;
+
+            Arrays.sort(arr);
+
+            if(arr[0]*arr[0] + arr[1]*arr[1] == arr[2]*arr[2])
+                str.append("right\n");
+            else
+                str.append("wrong\n");
         }
+
         System.out.print(str);
         br.close();
     }
