e618b0cd9f014c58fbd59a68771750b047be0bd7
HanHoon <hoongrammer@gmail.com>
2023-05-16 10:49:39 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B2_15829_Hashing.java
+++ src/Lv1_Bronze/BOJ_B2_15829_Hashing.java
@@ -1,14 +1,13 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+import java.math.*;
 
 /**
  * @author HanHoon
  * @category .
- * https://www.acmicpc.net/problem/.
+ * https://www.acmicpc.net/problem/15829
  */
 public class BOJ_B2_15829_Hashing {
     public static void main(String[] args) throws IOException {
@@ -16,14 +15,18 @@ public class BOJ_B2_15829_Hashing {
         StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        // L
+        int L = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+        BigInteger result = new BigInteger("0");
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        for (int i = 0; i < L; i++) {
+            result = result.add(BigInteger.valueOf(S.charAt(i) - 96)
+                    .multiply(BigInteger.valueOf(31).pow(i)));
         }
+
+        str.append(result.remainder(BigInteger.valueOf(1234567891)));
+
         System.out.print(str);
         br.close();
     }
