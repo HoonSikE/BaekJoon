0078446cef9f9ca293f84004f716296602769e7c
HanHoon <hoongrammer@gmail.com>
2023-05-17 11:15:24 +0900

    Update BOJ 15439

--- src/Lv1_Bronze/BOJ_B4_15439_베라의_패션.java
+++ src/Lv1_Bronze/BOJ_B4_15439_베라의_패션.java
@@ -1,29 +1,22 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 수학, 구현, 조합론
+ * https://www.acmicpc.net/problem/15439
  */
 public class BOJ_B4_15439_베라의_패션 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
-        }
+        str.append(N*(N-1));
+
         System.out.print(str);
         br.close();
     }
