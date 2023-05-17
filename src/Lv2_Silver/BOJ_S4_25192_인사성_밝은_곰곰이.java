93fae1410f2aceb44b20c0fcd44f491091f53a5f
HanHoon <hoongrammer@gmail.com>
2023-05-17 11:43:50 +0900

    Udpaet BOJ 25192

--- src/Lv2_Silver/BOJ_S4_25192_인사성_밝은_곰곰이.java
+++ src/Lv2_Silver/BOJ_S4_25192_인사성_밝은_곰곰이.java
@@ -1,29 +1,37 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 자료 구조, 해시를 사용한 집합과 맵, 트리를 사용한 집합과 맵
+ * https://www.acmicpc.net/problem/25192
  */
 public class BOJ_S4_25192_인사성_밝은_곰곰이 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        int ans = 0;
 
         for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            String s = br.readLine();
+            if(s.equals("ENTER")) {
+                set.clear();
+                continue;
+            }
+            if(!set.contains(s)){
+                set.add(s);
+                ans++;
+            }
         }
+
+        str.append(ans);
+
         System.out.print(str);
         br.close();
     }
--- src/Lv2_Silver/BOJ_S5_1010_다리_놓기.java
+++ src/Lv2_Silver/BOJ_S5_1010_다리_놓기.java
@@ -1,14 +1,12 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 수학, 다이나믹 프로그래밍, 조합론
+ * https://www.acmicpc.net/problem/1010
  */
 public class BOJ_S5_1010_다리_놓기 {
     public static void main(String[] args) throws IOException {
@@ -16,13 +14,13 @@ public class BOJ_S5_1010_다리_놓기 {
         StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        // T: 테스트 케이스의 개수
+        int T = Integer.parseInt(br.readLine());
 
-        for (int n = 0; n < N; n++){
+        for (int t = 0; t < T; t++){
             st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
         }
         System.out.print(str);
         br.close();
