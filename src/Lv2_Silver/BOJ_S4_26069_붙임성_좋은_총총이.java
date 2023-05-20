a2e489b6c943936b34eca39d66bad59716c53438
HanHoon <hoongrammer@gmail.com>
2023-05-20 21:42:12 +0900

    Update BOJ 2023-05-20

--- src/Lv2_Silver/BOJ_S4_26069_붙임성_좋은_총총이.java
+++ src/Lv2_Silver/BOJ_S4_26069_붙임성_좋은_총총이.java
@@ -1,14 +1,12 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 자료 구조, 해시를 사용한 집합과 맵, 트리를 사용한 집합과 맵
+ * https://www.acmicpc.net/problem/26069
  */
 public class BOJ_S4_26069_붙임성_좋은_총총이 {
     public static void main(String[] args) throws IOException {
@@ -19,11 +17,22 @@ public class BOJ_S4_26069_붙임성_좋은_총총이 {
         // N
         int N = Integer.parseInt(br.readLine());
 
+        HashSet<String> set = new HashSet<>();
+        set.add("ChongChong");
+
         for (int n = 0; n < N; n++){
             st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            String s1 = st.nextToken();
+            String s2 = st.nextToken();
+
+            if(set.contains(s1))
+                set.add(s2);
+            else if(set.contains(s2))
+                set.add(s1);
         }
+
+        str.append(set.size());
+
         System.out.print(str);
         br.close();
     }
