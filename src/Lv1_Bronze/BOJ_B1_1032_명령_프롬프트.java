23276e6c623057817eac6a65f65d71c287f05df4
HanHoon <hoongrammer@gmail.com>
2023-05-20 22:00:59 +0900

    Update BOJ 1032

--- src/Lv1_Bronze/BOJ_B1_1032_명령_프롬프트.java
+++ src/Lv1_Bronze/BOJ_B1_1032_명령_프롬프트.java
@@ -1,29 +1,47 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 구현, 문자열
+ * https://www.acmicpc.net/problem/1032
  */
 public class BOJ_B1_1032_명령_프롬프트 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+
+        // 이름의 길이는 모두 같고 길이는 최대 50이다.
+        for (int n = 0; n < N; n++)
+            arr[n] = br.readLine();
+
+        String result = "";
+
+        for(int i = 0; i < arr[0].length(); i++){
+            char c = arr[0].charAt(i);
+            boolean flag = true;
+
+            for(int n = 1; n < arr.length; n++){
+                if(c != arr[n].charAt(i)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag)
+                result += c;
+            else
+                result += "?";
         }
+
+        str.append(result);
+
         System.out.print(str);
         br.close();
     }
