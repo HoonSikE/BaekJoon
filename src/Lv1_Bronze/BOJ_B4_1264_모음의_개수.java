e29429f1a4b26b3cf91f8f783a7aa26c148f15de
HanHoon <hoongrammer@gmail.com>
2023-06-04 17:06:54 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B4_1264_모음의_개수.java
+++ src/Lv1_Bronze/BOJ_B4_1264_모음의_개수.java
@@ -1,14 +1,12 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 구현, 문자열
+ * https://www.acmicpc.net/problem/1264
  */
 public class BOJ_B4_1264_모음의_개수 {
     public static void main(String[] args) throws IOException {
