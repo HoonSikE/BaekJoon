0b18d82aa50325a58efab5e58e5cf1731a357892
HanHoon <hoongrammer@gmail.com>
2023-06-08 21:13:41 +0900

    Update BOJ

--- src/Lv2_Silver/BOJ_S4_11047_동전_0.java
+++ src/Lv2_Silver/BOJ_S4_11047_동전_0.java
@@ -1,29 +1,38 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 /**
  * @author HanHoon
  * @category 그리디 알고리즘
- * https://www.acmicpc.net/problem/.
+ * https://www.acmicpc.net/problem/11047
  */
 public class BOJ_S4_11047_동전_0 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
         // N
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        // N개의 동전 종류
+        int[] arr = new int[N];
+        for (int n = 0; n < N; n++)
+            arr[n] = Integer.parseInt(br.readLine());
+
+        int result = 0;
+        for(int n = N-1; n >= 0; n--){
+            if(K == 0)
+                break;
+            result += K / arr[n];
+            K = K % arr[n];
         }
+
+        str.append(result);
+
         System.out.print(str);
         br.close();
     }
