244db83a1d087725469a4ed738ba6bb9189fd6c4
HanHoon <hoongrammer@gmail.com>
2023-06-15 10:59:57 +0900

    Update BOJ

--- src/Lv3_Gold/BOJ_G5_2293_동전_1.java
+++ src/Lv3_Gold/BOJ_G5_2293_동전_1.java
@@ -1,29 +1,41 @@
 package Lv3_Gold;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 다이나믹 프로그래밍
+ * https://www.acmicpc.net/problem/2293
  */
 public class BOJ_G5_2293_동전_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
         // N
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        int[] dp = new int[K+1];
+        int[] coin = new int[N];
+
+        for (int n = 0; n < N; n++)
+            coin[n] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(coin);
+        dp[0] = 1;
+
+        for(int n = 0; n < N; n++){
+            for(int k = 1; k <= K; k++){
+                if(k >= coin[n])
+                    dp[k] += dp[k - coin[n]];
+            }
         }
+
+        str.append(dp[K]);
+
         System.out.print(str);
         br.close();
     }
