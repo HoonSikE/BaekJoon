3fbae0f110337437e074d2eeb7948f0a351faf8e
HanHoon <hoongrammer@gmail.com>
2023-06-26 21:49:08 +0900

    Update BOJ

--- src/Lv2_Silver/BOJ_S3_4779_칸토어_집합.java
+++ src/Lv2_Silver/BOJ_S3_4779_칸토어_집합.java
@@ -1,7 +1,6 @@
 package Lv2_Silver;
 
 import java.io.*;
-import java.util.*;
 
 /**
  * @author HanHoon
@@ -11,15 +10,12 @@ import java.util.*;
 public class BOJ_S3_4779_칸토어_집합 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
 
         for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
             Integer.parseInt(br.readLine());
         }
         System.out.print(str);
