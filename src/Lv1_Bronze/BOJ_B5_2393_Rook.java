754c47832613592f92be99ea9b57f1d372bac4ac
HanHoon <hoongrammer@gmail.com>
2023-07-08 11:02:49 +0900

    Udpaet BOJ

---
+++ src/Lv1_Bronze/BOJ_B5_2393_Rook.java
@@ -0,0 +1,32 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/2393
+ */
+public class BOJ_B5_2393_Rook {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder str = new StringBuilder();
+
+        str.append("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+
+        System.out.print(str);
+        br.close();
+    }
+}
