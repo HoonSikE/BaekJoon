1dde082f1f05e98c965be0f0dd9c9b41d0cfe752
HanHoon <hoongrammer@gmail.com>
2023-07-10 13:48:00 +0000

    Update BOJ

---
+++ src/Lv1_Bronze/BOJ_B5_5338_마이크로소프트_로고.java
@@ -0,0 +1,22 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/5338
+ */
+public class BOJ_B5_5338_마이크로소프트_로고 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        str.append("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+
+        System.out.print(str);
+    }
+}
