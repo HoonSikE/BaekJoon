5307c121b14a7c925294720f9ced1056a121e9b7
HanHoon <hoongrammer@gmail.com>
2023-07-12 11:13:43 +0900

    Update BOJ

---
+++ src/Lv1_Bronze/BOJ_B4_4470_줄번호.java
@@ -0,0 +1,24 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현, 문자열
+ * https://www.acmicpc.net/problem/4470
+ */
+public class BOJ_B4_4470_줄번호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder str = new StringBuilder();
+
+        // N
+        int N = Integer.parseInt(br.readLine());
+
+        for (int n = 1; n <= N; n++)
+            str.append(n + ". " + br.readLine() + "\n");
+
+        System.out.print(str);
+        br.close();
+    }
+}
