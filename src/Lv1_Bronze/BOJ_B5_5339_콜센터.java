81c0f2a3ac18e31f8db440f14477efae1c9aed59
HanHoon <hoongrammer@gmail.com>
2023-07-14 21:25:42 +0900

    Update BOJ 2023-07-14

---
+++ src/Lv1_Bronze/BOJ_B5_5339_콜센터.java
@@ -0,0 +1,27 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/5339
+ */
+public class BOJ_B5_5339_콜센터 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        str.append("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+
+        System.out.print(str);
+    }
+}
