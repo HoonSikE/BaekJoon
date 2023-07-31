9beba452a4afe6e01f7bb6e839ed9ab86083c352
HanHoon <hoongrammer@gmail.com>
2023-07-31 18:23:58 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B2_1297_TV_크기.java
+++ src/Lv1_Bronze/BOJ_B2_1297_TV_크기.java
@@ -1,29 +1,28 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 기하학, 피타고라스 정리
+ * https://www.acmicpc.net/problem/1297
  */
 public class BOJ_B2_1297_TV_크기 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        double D = Double.parseDouble(st.nextToken());
+        double H = Double.parseDouble(st.nextToken());
+        double W = Double.parseDouble(st.nextToken());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
-        }
+        double pow = Math.pow(D, 2) / (Math.pow(H, 2) + Math.pow(W, 2));
+        double sqrt = Math.sqrt(pow);
+
+        str.append((int)(sqrt*H) + " " + (int)(sqrt*W));
+
         System.out.print(str);
         br.close();
     }
