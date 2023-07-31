b40db0a6f08e10273599664b0f5391e485496367
HanHoon <hoongrammer@gmail.com>
2023-07-31 18:17:22 +0900

    Udpate BOJ

--- src/Lv1_Bronze/BOJ_B2_1252_이진수_덧셈.java
+++ src/Lv1_Bronze/BOJ_B2_1252_이진수_덧셈.java
@@ -1,29 +1,32 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.math.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 수학, 구현, 사칙연산
+ * https://www.acmicpc.net/problem/1252
  */
 public class BOJ_B2_1252_이진수_덧셈 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        String s1 = st.nextToken();
+        String s2 = st.nextToken();
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
-        }
+        // BigInteger 형변환하면서 10진수로 바꿔줌
+        BigInteger S1 = new BigInteger(s1, 2);
+        BigInteger S2 = new BigInteger(s2, 2);
+
+        BigInteger sum = S1.add(S2);
+
+        // 2진수 변환
+        str.append(sum.toString(2));
+
         System.out.print(str);
         br.close();
     }
