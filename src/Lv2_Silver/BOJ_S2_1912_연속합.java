e5e83c7343df9769fcb3a5baaea154fe5b39cdee
HanHoon <hoongrammer@gmail.com>
2023-08-01 15:38:58 +0900

    Update BOJ

--- src/Lv2_Silver/BOJ_S2_1912_연속합.java
+++ src/Lv2_Silver/BOJ_S2_1912_연속합.java
@@ -1,14 +1,12 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 다이나믹 프로그래밍
+ * https://www.acmicpc.net/problem/1912
  */
 public class BOJ_S2_1912_연속합 {
     public static void main(String[] args) throws IOException {
@@ -19,11 +17,25 @@ public class BOJ_S2_1912_연속합 {
         // N
         int N = Integer.parseInt(br.readLine());
 
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        st = new StringTokenizer(br.readLine());
         for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+            arr[n] = Integer.parseInt(st.nextToken());
         }
+
+        dp[0] = arr[0];
+        int answer = arr[0];
+
+        for(int n = 1; n < N; n++){
+            dp[n] = Math.max(dp[n-1] + arr[n], arr[n]);
+
+            answer = Math.max(answer, dp[n]);
+        }
+
+        str.append(answer);
+
         System.out.print(str);
         br.close();
     }
