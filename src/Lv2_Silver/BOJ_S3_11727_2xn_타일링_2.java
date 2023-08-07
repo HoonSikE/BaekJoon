81aa296c505600b1875462c8ac63c6b2d7b96254
HanHoon <hoongrammer@gmail.com>
2023-08-07 18:56:37 +0900

    Update BOJ 2023-08-07

--- src/Lv2_Silver/BOJ_S3_11727_2xn_타일링_2.java
+++ src/Lv2_Silver/BOJ_S3_11727_2xn_타일링_2.java
@@ -1,7 +1,6 @@
 package Lv2_Silver;
 
 import java.io.*;
-import java.util.*;
 
 /**
  * @author HanHoon
@@ -11,17 +10,21 @@ import java.util.*;
 public class BOJ_S3_11727_2xn_타일링_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
+        int[] dp = new int [1001];
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int n = 3; n <= N; n++){
+            dp[n] = (dp[n-2]*2 + dp[n-1])%10007;
         }
+
+        str.append(dp[N]);
+
         System.out.print(str);
         br.close();
     }
