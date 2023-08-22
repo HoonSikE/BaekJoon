36068f5f2bcd7e0ca50123df8a03efe484e18399
HanHoon <hoongrammer@gmail.com>
2023-08-22 18:00:38 +0900

    Update BOJ

--- src/Lv1_Bronze/BOJ_B2_1440_타임머신.java
+++ src/Lv1_Bronze/BOJ_B2_1440_타임머신.java
@@ -14,14 +14,30 @@ public class BOJ_B2_1440_타임머신 {
         StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        String time = br.readLine();
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        st = new StringTokenizer(time, ":");
+
+        int[] arr = new int[3];
+
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+
+        for(int h = 0; h < 3; h++){
+            for(int m = 0; m < 3; m++) {
+                for (int s = 0; s < 3; s++) {
+                    if(h != m && m != s && s != h)
+                        if(arr[h] >= 1 && arr[h] <= 12 && arr[m] >= 0 && arr[m] <= 59 && arr[s] >= 0 && arr[s] <= 59)
+                            cnt++;
+                }
+            }
         }
+
+        str.append(cnt);
+
         System.out.print(str);
         br.close();
     }
