23e92b09b67baa23acc84d1066566ea523514b75
HanHoon <hoongrammer@gmail.com>
2023-12-28 00:10:42 +0900

    udpate BOJ 2023-12-28

--- src/Lv2_Silver/BOJ_S1_2667_단지번호붙이기.java
+++ src/Lv2_Silver/BOJ_S1_2667_단지번호붙이기.java
@@ -1,11 +1,7 @@
 package Lv2_Silver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author HanHoon
@@ -13,42 +9,31 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/2667
  */
 public class BOJ_S1_2667_단지번호붙이기 {
-    static int N;
+    static int N, count;
     static char[][] map;
     static boolean[][] isVisited;
     // 상하좌우
     static int[] dCol = {-1, 1, 0, 0 };
     static int[] dRow = { 0, 0,-1, 1 };
-    public static int bfs(int[] xy){
-        int count = 0;
+    public static void dfs(int y, int x){
+        // 상하좌우
+        for(int i = 0; i < 4; i++){
+            int nextY = y + dCol[i];
+            int nextX = x + dRow[i];
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(xy);
+            // 범위 체크
+            if(nextY < 0 || nextY >= N || nextX < 0 || nextX >= N)
+                continue;
 
-        while(!queue.isEmpty()){
-            int[] start = queue.poll();
+            // 0이거나 방문했다면 continue;
+            if(map[nextY][nextX] == '0' || isVisited[nextY][nextX])
+                continue;
 
-            // 상하좌우
-            for(int i = 0; i < 4; i++){
-                int nextY = start[0] + dCol[i];
-                int nextX = start[1] + dRow[i];
-
-                // 범위 체크
-                if(nextY < 0 || nextY >= N || nextX < 0 || nextX >= N)
-                    continue;
-
-                // 0이거나 방문했다면 continue;
-                if(map[nextY][nextX] == 0 || isVisited[nextY][nextX])
-                    continue;
-
-                // 방문 체크
-                isVisited[nextY][nextX] = true;
-                count++;
-                queue.offer(new int[] {nextY, nextX});
-            }
+            // 방문 체크
+            isVisited[nextY][nextX] = true;
+            count++;
+            dfs(nextY, nextX);
         }
-
-        return count;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -65,19 +50,28 @@ public class BOJ_S1_2667_단지번호붙이기 {
             map[n] = br.readLine().toCharArray();
         }
 
+        ArrayList<Integer> list = new ArrayList<>();
+
         for (int n = 0; n < N; n++){
             for (int m = 0; m < N; m++){
                 // 0이거나 방문했다면 continue;
-                if(map[n][m] == 0 || isVisited[n][m])
+                if(map[n][m] == '0' || isVisited[n][m])
                     continue;
-
-//                int num = bfs(new int[] {n, m});
-//                if(num > 0){
-                    str.append(bfs(new int[] {n, m})).append("\n");
-//                }
-                str.append("ddd" + n + "ddd" + m).append("\n");
+                count = 1;
+                isVisited[n][m] = true;
+                dfs(n, m);
+                list.add(count);
             }
         }
+
+        Collections.sort(list);
+
+        str.append(list.size()).append("\n");
+
+        for(int num : list){
+            str.append(num).append("\n");
+        }
+
         System.out.print(str);
         br.close();
     }
