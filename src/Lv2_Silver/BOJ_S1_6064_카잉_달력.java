b1d8c82f23b0269a72d9ea39f595d2333f5928ca
HanHoon <hoongrammer@gmail.com>
2023-12-29 23:59:34 +0900

    Update BOJ 2023-12-29

---
+++ src/Lv2_Silver/BOJ_S1_6064_카잉_달력.java
@@ -0,0 +1,43 @@
+package Lv2_Silver;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author HanHoon
+ * @category 수학, 브루트포스 알고리즘, 정수론, 중국인의 나머지 정리
+ * https://www.acmicpc.net/problem/6064
+ */
+public class BOJ_S1_6064_카잉_달력 {
+    static int M, N, x, y;
+    public static int solution(){
+        boolean check = false;
+
+        for (int i = x; i < (N * M); i += M) {
+            if (i % N == y) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder str = new StringBuilder();
+
+        // T
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++){
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+
+            str.append(solution()).append("\n");
+        }
+        System.out.print(str);
+        br.close();
+    }
+}
