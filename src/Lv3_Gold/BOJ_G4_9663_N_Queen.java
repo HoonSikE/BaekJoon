cc64c76bf8d4c8b2cc70c9677e065a2f65891117
HanHoon <hoongrammer@gmail.com>
2024-01-03 23:10:37 +0900

    Update BOJ 2024-01-03

---
+++ src/Lv3_Gold/BOJ_G4_9663_N_Queen.java
@@ -0,0 +1,81 @@
+package Lv3_Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author HanHoon
+ * @category .
+ * https://www.acmicpc.net/problem/.
+ */
+public class BOJ_G4_9663_N_Queen {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = null;
+//        StringBuilder str = new StringBuilder();
+//
+//        // N
+//        int N = Integer.parseInt(br.readLine());
+//
+//        for (int n = 0; n < N; n++){
+//            st = new StringTokenizer(br.readLine());
+//            Integer.parseInt(st.nextToken());
+//            Integer.parseInt(br.readLine());
+//        }
+//        System.out.print(str);
+//        br.close();
+//    }
+public static int[] arr;
+    public static int N;
+    public static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        nQueen(0);
+        System.out.println(count);
+
+    }
+
+    public static void nQueen(int depth) {
+        // 모든 원소를 다 채운 상태면 count 증가 및 return
+        if (depth == N) {
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            arr[depth] = i;
+            // 놓을 수 있는 위치일 경우 재귀호출
+            if (Possibility(depth)) {
+                nQueen(depth + 1);
+            }
+        }
+
+    }
+
+    public static boolean Possibility(int col) {
+
+        for (int i = 0; i < col; i++) {
+            // 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우)
+            if (arr[col] == arr[i]) {
+                return false;
+            }
+
+            /*
+             * 대각선상에 놓여있는 경우
+             * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
+             */
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
