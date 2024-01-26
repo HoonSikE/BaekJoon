b26d3ef0b1029ca925380c2b6e32d211796bac22
HanHoon <hoongrammer@gmail.com>
2024-01-27 00:15:32 +0900

    Update BOJ 2024-01-27

--- src/Lv1_Bronze/BOJ_B2_2355_시그마.java
+++ src/Lv1_Bronze/BOJ_B2_2355_시그마.java
@@ -1,29 +1,34 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
 
 /**
  * @author HanHoon
- * @category .
- * https://www.acmicpc.net/problem/.
+ * @category 수학, 사칙연산
+ * https://www.acmicpc.net/problem/2355
  */
 public class BOJ_B2_2355_시그마 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder str = new StringBuilder();
 
-        // N
-        int N = Integer.parseInt(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
+        long N = Math.min(A, B);
+        long M = Math.max(A, B);
+
+        long sum = (N+M) * ((M-N+1)/2);
+
+        if((M-N+1)%2 != 0){
+            sum += (N + M) / 2;
         }
+
+        str.append(sum);
+
         System.out.print(str);
         br.close();
     }
