514e445073106d52633efdd72bcab13ebb0468d1
HanHoon <hoongrammer@gmail.com>
2024-02-15 23:09:26 +0900

    Update BOJ 2024-02-15

---
+++ src/Lv3_Gold/BOJ_G5_1013_Contact.java
@@ -0,0 +1,30 @@
+package Lv3_Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @author HanHoon
+ * @category .
+ * https://www.acmicpc.net/problem/.
+ */
+public class BOJ_G5_1013_Contact {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder str = new StringBuilder();
+
+        // N
+        int N = Integer.parseInt(br.readLine());
+
+        for (int n = 0; n < N; n++){
+            st = new StringTokenizer(br.readLine());
+            Integer.parseInt(st.nextToken());
+            Integer.parseInt(br.readLine());
+        }
+        System.out.print(str);
+        br.close();
+    }
+}
