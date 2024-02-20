f502683b844bbd26b4fde01a7f146b60dad28368
HanHoon <hoongrammer@gmail.com>
2024-02-20 23:37:31 +0900

    Update BOJ 2024-02-20

---
+++ src/Lv1_Bronze/BOJ_B5_11942_고려대는_사랑입니다.java
@@ -0,0 +1,18 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/11942
+ */
+public class BOJ_B5_11942_고려대는_사랑입니다 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        str.append("고려대학교");
+
+        System.out.print(str);
+    }
+}
