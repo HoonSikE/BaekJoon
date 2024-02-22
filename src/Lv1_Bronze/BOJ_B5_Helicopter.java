3e3a9acb5b392460f01a222ea32fe036ca72bb43
HanHoon <hoongrammer@gmail.com>
2024-02-22 22:47:35 +0900

    Update BOJ 2024-02-22

---
+++ src/Lv1_Bronze/BOJ_B5_Helicopter.java
@@ -0,0 +1,25 @@
+package Lv1_Bronze;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/14337
+ */
+public class BOJ_B5_Helicopter {
+    public static void main(String[] args) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        str.append("      ===================\n" +
+                "          ____||___\n" +
+                "\\ /      /       []\\\n" +
+                " X=======           \\__\n" +
+                "/ \\      \\____________|\n" +
+                "            ||  ||\n" +
+                "         \\-----------/");
+
+        System.out.print(str);
+    }
+}
