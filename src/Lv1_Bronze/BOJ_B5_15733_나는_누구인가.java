f966a498b818efbe6d84c146af75baafa1b376a6
HanHoon <hoongrammer@gmail.com>
2024-02-29 18:35:31 +0900

    Update BOJ 2024-02-29

---
+++ src/Lv1_Bronze/BOJ_B5_15733_나는_누구인가.java
@@ -0,0 +1,18 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/15733
+ */
+public class BOJ_B5_15733_나는_누구인가 {
+    public static void main(String[] args) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        str.append("I'm Sexy");
+
+        System.out.print(str);
+    }
+}
