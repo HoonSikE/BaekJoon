fe2f05f06a782e0fc80cff75e26502e10977685a
HanHoon <hoongrammer@gmail.com>
2024-03-02 10:51:41 +0900

    Update BOJ 2024-03-02

---
+++ src/Lv1_Bronze/BOJ_B5_15962_새로운_시작.java
@@ -0,0 +1,12 @@
+package Lv1_Bronze;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/15962
+ */
+public class BOJ_B5_15962_새로운_시작 {
+    public static void main(String[] args) {
+        System.out.print("파이팅!!");
+    }
+}
