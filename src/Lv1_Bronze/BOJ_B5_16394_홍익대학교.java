e3a435f90bbb1b8ca46b0e31ff54e4b9898b0456
HanHoon <hoongrammer@gmail.com>
2024-03-07 23:18:29 +0900

    Update BOJ 2024-03-07

---
+++ src/Lv1_Bronze/BOJ_B5_16394_홍익대학교.java
@@ -0,0 +1,16 @@
+package Lv1_Bronze;
+
+import java.util.*;
+
+/**
+ * @author HanHoon
+ * @category 수학, 사칙연산
+ * https://www.acmicpc.net/problem/16394
+ */
+public class BOJ_B5_16394_홍익대학교 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N-1946);
+    }
+}
