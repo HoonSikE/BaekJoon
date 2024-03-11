63d7e6ac9091f6c022cb08d5e4096298256f9816
HanHoon <hoongrammer@gmail.com>
2024-03-11 23:58:55 +0900

    update BOJ 2024-03-11

---
+++ src/Lv1_Bronze/BOJ_B5_18096.java
@@ -0,0 +1,22 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 수학
+ * https://www.acmicpc.net/problem/18096
+ */
+public class BOJ_B5_18096 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder str = new StringBuilder();
+
+        // N
+        int N = Integer.parseInt(br.readLine());
+        str.append(1);
+
+        System.out.print(str);
+        br.close();
+    }
+}
