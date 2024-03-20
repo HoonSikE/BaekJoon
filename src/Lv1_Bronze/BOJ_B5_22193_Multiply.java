ba5c6282258702f9a3a88791163ff4fcd5c69d3c
HanHoon <hoongrammer@gmail.com>
2024-03-20 23:49:07 +0900

    Update BOJ 2024-03-20

--- src/Lv1_Bronze/BOJ_B5_22193_Multiply.java
+++ src/Lv1_Bronze/BOJ_B5_22193_Multiply.java
@@ -1,10 +1,8 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.math.*;
+import java.util.*;
 
 /**
  * @author HanHoon
---
+++ src/Lv1_Bronze/BOJ_B5_23234_The_World_Responds.java
@@ -0,0 +1,18 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/23234
+ */
+public class BOJ_B5_23234_The_World_Responds {
+    public static void main(String[] args) throws IOException {
+        StringBuilder str = new StringBuilder();
+
+        str.append("The world says hello!");
+
+        System.out.print(str);
+    }
+}
