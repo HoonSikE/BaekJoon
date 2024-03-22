4daa60d1bd0fe6a3e095353eeb8fb379afeda0bc
HanHoon <hoongrammer@gmail.com>
2024-03-22 23:29:40 +0900

    Update BOJ 2024-03-22

--- src/Lv1_Bronze/BOJ_B5_24078_Remainder.java
+++ src/Lv1_Bronze/BOJ_B5_24078_Remainder.java
@@ -1,29 +1,22 @@
 package Lv1_Bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
 
 /**
  * @author HanHoon
- * @category .
+ * @category 수학, 사칙연산
  * https://www.acmicpc.net/problem/24078
  */
 public class BOJ_B5_24078_Remainder {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
         StringBuilder str = new StringBuilder();
 
         // N
         int N = Integer.parseInt(br.readLine());
 
-        for (int n = 0; n < N; n++){
-            st = new StringTokenizer(br.readLine());
-            Integer.parseInt(st.nextToken());
-            Integer.parseInt(br.readLine());
-        }
+        str.append(N%21);
+
         System.out.print(str);
         br.close();
     }
