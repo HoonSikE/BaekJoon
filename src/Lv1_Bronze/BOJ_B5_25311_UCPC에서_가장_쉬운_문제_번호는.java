a4cf469bfb48504e0cfe5d3a83cbeb180d3e3d8b
HanHoon <hoongrammer@gmail.com>
2024-03-30 22:44:11 +0900

    Update BOJ 2024-03-30

---
+++ src/Lv1_Bronze/BOJ_B5_25311_UCPC에서_가장_쉬운_문제_번호는.java
@@ -0,0 +1,23 @@
+package Lv1_Bronze;
+
+import java.io.*;
+
+/**
+ * @author HanHoon
+ * @category 구현
+ * https://www.acmicpc.net/problem/25311
+ */
+public class BOJ_B5_25311_UCPC에서_가장_쉬운_문제_번호는 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder str = new StringBuilder();
+
+        // N
+        int y = Integer.parseInt(br.readLine());
+
+        str.append("A");
+
+        System.out.print(str);
+        br.close();
+    }
+}
